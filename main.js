158aa18009bd2cfcb77e36f7b17c580fcc12b7be
rizayeva09 <erkinovasalomat552@gmail.com>
2024-10-24 12:08:18 +0500

    FLAGS

--- main.js
+++ main.js
@@ -1,42 +1,6 @@
 
 let count = document.getElementById('count')
 let inp = document.getElementById('inp')
-
-// function data(calback){
-//     const getData = new XMLHttpRequest()
-//     getData.addEventListener('readystatechange',()=>{
-//         if(getData.readyState === 4 && getData.status === 200){
-//         const jsonData = JSON.parse(getData.responseText)
-       
-
-      
-//         calback(jsonData, undefined)
-        
-//     }else{
-//         calback(undefined, 'xato')
-//     }
-// })
-
-    
-//     getData.open
-//     getData.send()
-// }
-
-
-// data((data, err)=>{   
-//     console.log(data);
-     
-    
-// if(err) {
-//     console.log(err);
-    
-// }else{
-//     data.forEach((item)=>{
-
-//         
-//         })
-// }
-// })
 inp.addEventListener('input', ()=>{
     
     let val = inp.value.toLowerCase()
